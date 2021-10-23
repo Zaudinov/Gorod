70e61633baa916cc17af12816027f0a1a100a120
Zaudinov <https://github.com/Zaudinov/Spring.IoC>
2021-10-23 17:22:27 +0700

    Initial commit

---
+++ src/main/java/com/gorod/testcase/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gorod.testcase;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestcaseApplication.class);
+	}
+
+}
