d5aac4f8c951ebb7a5da343bdf52f0c2edb2074f
Zaudinov <https://github.com/Zaudinov/Spring.IoC>
2021-10-30 19:35:26 +0700

    work stopped commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,27 +17,3 @@ CREATE TABLE IF NOT EXISTS subscriber_service(
     service_id int foreign key references service(service_id) on delete cascade on update cascade
 );
 
-INSERT INTO SUBSCRIBER (SUBSCRIBER_ID, ACCOUNT, FIO) VALUES
-(1, '1235213', 'ZII'),
-(2, '2235213', 'PVV');
-
-INSERT INTO SERVICE (SERVICE_ID,name, PARENT_ID) VALUES
-(1, 'Жилищно-коммунальные услуги', null),
-(2, 'Отопление', 1),
-(3, 'Вода',1),
-(4, 'Горячяя Вода',3),
-(5, 'Холодная Вода',3),
-(6, 'Детский сад', null),
-(7, 'Ясли', 6);
-
-
-INSERT INTO SUBSCRIBER_SERVICE (SUBSCRIBER_ID, SERVICE_ID) VALUES
-(1, 1),
-(1, 5),
-(1,6),
-(1,7),
-(2, 2),
-(2, 3),
-(2, 4),
-(2, 5);
-
